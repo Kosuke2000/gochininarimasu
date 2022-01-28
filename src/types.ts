42b84346e93bc82c7c1183347d3726015d4db32b
Kosuke2000 <youuchiharu@gmail.com>
2022-01-28 17:36:40 +0900

    declear type

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type Member = {
+  nickname: string;
+  paidAmount: number;
+};
+
+export const SEIDAN_ALL_MEMBERS: Member[] = [
+  { nickname: "Hajime", paidAmount: 0 },
+  { nickname: "Hiromu", paidAmount: 0 },
+  { nickname: "Kosuke", paidAmount: 0 },
+];
